7658c70a7793ff897b1dabb1b29e9c27ce3557af
jrthompson25 <jason.r.thompson25@gmail.com>
2020-01-27 15:10:48 -0600

    Add files via upload

---
+++ animations.js
@@ -0,0 +1,9 @@
+$("#menu_button").on("click", function() {
+  $("#menu").show();
+  $("#menu_button").hide();
+});
+$("#dismiss").on("click", function() {
+  $("#menu").hide();
+  $("#menu_button").show();
+});
+  
